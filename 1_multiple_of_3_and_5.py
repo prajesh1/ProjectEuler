c0b46bcacf46c379d56f5ef62e223a8a7903eb7a
Prajesh Ravindran <prajeshr@amazon.com>
2020-04-24 23:43:17 -0700

    1. Multiple of 3 and 5 within 1000

---
+++ 1_multiple_of_3_and_5.py
@@ -0,0 +1,8 @@
+def compute():
+    sum_of_matching_numbers = 0
+    for i in range(1000):
+        if i % 3 == 0 or i % 5 == 0:
+            sum_of_matching_numbers = sum_of_matching_numbers + i
+    print(sum_of_matching_numbers)
+
+compute()
